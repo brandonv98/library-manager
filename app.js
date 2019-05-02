c220ab089df49a81b0fb34aaf0bb54c418e0f053
Brandon VanCamp <brandon@overtimewebdev.com>
2019-05-02 08:49:52 -0500

    Update

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/index', routes);
 app.use('/books', books);
 app.use('/novels', novels);
 
