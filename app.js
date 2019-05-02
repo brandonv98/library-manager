923edb01d474f3aa644cc8a5058fd4cc3f2146f9
Brandon VanCamp <brandon@overtimewebdev.com>
2019-05-02 10:57:11 -0500

    n/a

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
+// TODO: uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(methodOverride('_method'));
 app.use(logger('dev'));
